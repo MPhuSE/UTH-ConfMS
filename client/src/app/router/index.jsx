f6da7395967d64b477dd693a4bb23e2aa9922840
nam335 <namnth5517@ut.edu.vn>
2026-01-05 10:26:06 +0700

    a

--- client/src/app/router/index.jsx
+++ client/src/app/router/index.jsx
@@ -1,81 +1,60 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+//layouts
 import PublicLayout from "../layouts/PublicLayout";
 import DashboardLayout from "../layouts/DashboardLayout";
-import ProtectedRoute from "./ProtectedRoute";
 
-// Public
+
 import CfpPublicPage from "../../features/auth/pages/CfpPublicPage";
 import Login from "../../features/auth/pages/Login";
 import Register from "../../features/auth/pages/Register";
+import Home from "../../features/dashboard/pages/Home";
 import ForgotPassword from "../../features/auth/pages/ForgotPassword";
-
-// Dashboard - Author
-import AuthorDashboard from "../../features/dashboard/AuthorDashboard";
 import AuthorProfile from "../../features/auth/pages/AuthorProfile";
-import PaperSubmissionPage from "../../features/submission/PaperSubmissionPage";
-import SubmissionDetailPage from "../../features/dashboard/pages/SubmissionDetailPage";
-
-// Admin / Chair (để sẵn)
 import SmtpConfig from "../../features/auth/pages/SmtpConfig";
 import DeadlineTrackConfig from "../../features/auth/pages/DeadlineTrackConfig";
 import ConferenceList from "../../features/auth/pages/ConferenceList";
 import TrackTopicManagement from "../../features/auth/pages/TrackTopicManagement";
+import PaperSubmissionPage from "../../features/submission/PaperSubmissionPage";
+
 import PcManagement from "../../features/auth/pages/PcManagement";
+
 import AuditLogs from "../../features/dashboard/pages/AuditLogs";
-import SubmissionManagement from "../../features/dashboard/pages/SubmissionManagement";
-import InternalDiscussion from "../../features/dashboard/pages/InternalDisscusion";
+import ReviewForm from "../../features/reviewer/ReviewForm";
+import AssignedPaper from "../../features/reviewer/AssignedPapers";
+import Proceeding from "../../features/dashboard/pages/ProceedingViews";
+
+
+
 
 export default function AppRouter() {
-  return (
-    <BrowserRouter>
-      <Routes>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<PublicLayout />}>
+                    <Route path="/" element={<CfpPublicPage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/forgotpassword" element={<ForgotPassword />} />
 
-        {/* PUBLIC */}
-        <Route element={<PublicLayout />}>
-          <Route path="/" element={<CfpPublicPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
-        </Route>
+                </Route>
+                <Route path="/dashboard" element={<DashboardLayout />}>
+                    <Route index element={<Home />} />
+                    <Route path="profile" element={<AuthorProfile />} />
+                    <Route path="audit-logs" element={<AuditLogs/>} />
+                    <Route path="smtp-config" element={<SmtpConfig />} />
+                    <Route path="deadline-config" element={<DeadlineTrackConfig />} />
+                    <Route path="conference-list" element={<ConferenceList />} />
+                    <Route path="track-topic" element={<TrackTopicManagement />} />
+                    <Route path="submission" element={<PaperSubmissionPage />} />
 
-        {/* AUTHOR DASHBOARD */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute allowRoles={["authors"]}>
-              <DashboardLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<AuthorDashboard />} />
-          <Route path="profile" element={<AuthorProfile />} />
-          <Route path="submission" element={<PaperSubmissionPage />} />
-          <Route path="submission/:id" element={<SubmissionDetailPage />} />
-        </Route>
+                    <Route path="review-form" element={<ReviewForm />} />
+                    <Route path="Assigned-paper" element={<AssignedPaper />} />
+                    <Route path="prooceedings" element={<Proceeding />} />
 
-        {/*CHAIR / ADMIN */}
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute allowRoles={["ADMIN", "CHAIR"]}>
-              <DashboardLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="audit-logs" element={<AuditLogs />} />
-          <Route path="smtp-config" element={<SmtpConfig />} />
-          <Route path="deadline-config" element={<DeadlineTrackConfig />} />
-          <Route path="conference-list" element={<ConferenceList />} />
-          <Route path="track-topic" element={<TrackTopicManagement />} />
-          <Route path="pc-management" element={<PcManagement />} />
-          <Route path="submission" element={<SubmissionManagement />} />
-          <Route path="submission/:id" element={<SubmissionDetailPage />} />
-          <Route path="submission/:id/discussion" element={<InternalDiscussion />} />
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
-  );
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
